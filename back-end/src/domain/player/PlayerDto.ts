a3671900f96f59ffb1dcc80ac96cc99172b59d8b
gustavoferrazrocha <gustavoguusfr@gmail.com>
2024-06-19 18:06:19 -0300

    refatorado funções da simulação

--- back-end/src/domain/player/PlayerDto.ts
+++ back-end/src/domain/player/PlayerDto.ts
@@ -1,9 +1,18 @@
 import { PlayerApiDto } from "./PlayerApiDto"
 
 export interface PlayerDto {
+    overall: number
+    dpm: number
+    fb: number
+    ks: number
+    dth: number
+    gd10: number
+    xpd10: number
+    csd10: number
     playerApiData: PlayerApiDto
     farm: number
     kills: number
     deaths: number
     assists: number
+    gold: number
 }
